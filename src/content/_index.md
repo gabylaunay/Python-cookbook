5d1cb998aa8b9f27cb45fd34defd8b286d4b52b4
galaunay <gaby.launay@tutanota.com>
2018-03-07 21:13:22 +0000

    Add Page description

--- src/content/_index.md
+++ src/content/_index.md
@@ -4,5 +4,8 @@ title = "Python CookBook"
 
 # Python Cookbook
 
-This website provides a collection of Python scripts, snippets and tutorial
-for data analysis.
+This website provides a collection of Python scripts, snippets and tutorials
+for scientific data analysis.
+The currently available ressource are listed below:
+
+{{% children style="card" depth="2"  description="true" %}}
--- src/export-notebooks.sh
+++ src/export-notebooks.sh
@@ -3,21 +3,32 @@
 notebook_dir="$(pwd)/notebooks"
 content_dir="$(pwd)/content"
 
+# Clean old generated jupyter notebooks
+for file in "$content_dir"; do
+    [ -d $file ] && rm -rf $file
+done
+
+# Generate new pages from jupyter notebooks
 cd $notebook_dir
 for file in *.ipynb; do
     basename=${file%.*}
     echo "=== Exporting $basename ==="
     # exporting to markdown
     jupyter nbconvert --to markdown "$file"
-    # Get page name from metadata
+    # Get page metadata
     page_name=$(jq -r ".metadata.hugo_page_name" "$basename.ipynb")
     if [[ $page_name = 'null' ]] ; then
         page_name=$basename
+    fi
+    page_description=$(jq -r ".metadata.hugo_description" "$basename.ipynb")
+    if [[ $page_description = 'null' ]] ; then
+        page_description=""
     fi
     # adding hugo header
     sed -i "1i+++" "${basename}.md"
     sed -i "1ititle = \"$page_name\"" "${basename}.md"
     sed -i "1iweight = 1" "${basename}.md"
+    sed -i "1idescription = \"$page_description\"" "${basename}.md"
     sed -i "1i+++" "${basename}.md"
     # Move to content folder
     mkdir -p "$content_dir/$basename"
